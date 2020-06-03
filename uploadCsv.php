9d5b0d9b68a535157b0ffecf429580fa5b386fd8
CarlosMagnoSTavares <carloscrls@hotmail.com>
2020-06-03 09:53:21 -0300

    Upload de CSV para tela
    o objetivo dessa página é possibilitar que você faça o upload de um CSV para inserir no banco ou trabalhar com o texto em tela sem a necessidade de salvar o arquivo fisico

---
+++ uploadCsv.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br,en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<head>
+	<title>UPLOAD CSV FILE</title>
+</head>
+	<body>
+	<form action="" method="post" accept-charset="utf-8" enctype="multipart/form-data">
+		<input type="file" name="file">
+		<input type="submit" name="btn_submit" value="Upload File" />
+	</form>
+
+	<?php
+		if (isset($_POST)) 
+		{
+			$fh = fopen($_FILES['file']['tmp_name'], 'r+');
+			$lines = array();
+			while( ($row = fgetcsv($fh)) !== FALSE ) 
+			{
+				$lines[] =$row;
+			}
+			echo "<pre>";
+			foreach ($lines as $key => $line) 
+			{
+				$result = utf8_encode($line[0]);
+				var_dump($result);
+			}
+		}
+	?>
+	</body>
+</html>
+
+<!-- 
+o objetivo dessa página é possibilitar que você faça o upload de um CSV para inserir no banco ou trabalhar com o texto em tela sem a necessidade de salvar o arquivo fisico 
+-->
